aa0df3f3106ea6ebd3e1ed17ee94b81908e7379c
Anna <annbudash@mail.ru>
2023-04-20 12:30:47 +0300

    Predicate

--- src/telran/util/List.java
+++ src/telran/util/List.java
@@ -1,5 +1,6 @@
 package telran.util;
 import java.util.Comparator;
+import java.util.function.Predicate;
 
 public interface List<T> extends Collection<T> {
 void add(int index, T obj);
@@ -9,4 +10,7 @@ int indexOf(T pattern);
 int lastIndexOf(T pattern);
 void sort();
 void sort(Comparator<T> comp);
+int indexOf(Predicate<T> predicate);
+int lastIndexOf(Predicate<T> predicate);
+boolean removeIf(Predicate<T> predicate);
 }
